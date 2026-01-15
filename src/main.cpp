385f23e75436c5d7a33a8a63c77f6ac986e762e0
kacastillo <kelleycastillo@outlook.com>
2026-01-15 20:22:06 +0000

    added a new button

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,11 @@ int main()
             bn::backdrop::set_color(bn::color(31, 11, 22));
         }
 
+        if (bn::keypad::b_pressed())
+        {
+            bn::backdrop::set_color(bn::color(12, 11, 2));
+        }
+
         bn::core::update();
     }
 }

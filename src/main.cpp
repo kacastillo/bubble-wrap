aca32e350d583b1a7c3c95c75d9a5d8b6206a00a
kacastillo <kelleycastillo@outlook.com>
2026-01-19 21:12:15 +0000

    added dots

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,16 @@
 int main()
 {
     bn::core::init();
+    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(10, 40);
+    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(-40, 40);
+    bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-30, 40);
+    bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(-20, 40);
+    bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-10, 40);
+    bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(0, 40);
+    bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(10, 40);
+    bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(20, 40);
+    bn::sprite_ptr myCircle8 = bn::sprite_items::dot.create_sprite(30, 40);
+    bn::sprite_ptr myCircle9 = bn::sprite_items::dot.create_sprite(40, 40);
 
     bn::backdrop::set_color(bn::color(21, 15, 14));
 
